a002bb99340539aafac004b00fcbfded0e702078
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 17:28:28 +0100

    ROR-25 #CG set up user repo

--- src/main/java/com/qa/roar/persistence/repository/UserRepo.java
+++ src/main/java/com/qa/roar/persistence/repository/UserRepo.java
@@ -1,5 +1,11 @@
 package com.qa.roar.persistence.repository;
 
-public class UserRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.roar.persistence.domain.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
 
 }
