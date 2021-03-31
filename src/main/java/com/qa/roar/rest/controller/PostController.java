c5e637f36e923909d8ead0f6bd22b1b0a3c4efcf
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 18:34:42 +0100

    ROR-30 #CG added annotations, variables and constructor to post controller

--- src/main/java/com/qa/roar/rest/controller/PostController.java
+++ src/main/java/com/qa/roar/rest/controller/PostController.java
@@ -1,5 +1,23 @@
 package com.qa.roar.rest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.roar.service.PostService;
+
+@RestController
+@RequestMapping("/posts")
+@CrossOrigin // this will need to be specified for security once we're set up on a server
 public class PostController {
 
+private PostService service;
+	
+	@Autowired
+	public PostController(PostService service) {
+		super();
+		this.service = service;
+	}
+	
 }
