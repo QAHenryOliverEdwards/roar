62ebb8ba1e51e4f70ecda0bda6c380322aef9fb7
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 17:14:02 +0100

    ROR-22 added variables to post domain

--- src/main/java/com/qa/roar/persistence/domain/Post.java
+++ src/main/java/com/qa/roar/persistence/domain/Post.java
@@ -1,5 +1,45 @@
 package com.qa.roar.persistence.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(max = 280)
+	private String body;
+	
+	@NotNull
+	private Boolean visibility;
+	
+	@ManyToOne
+	private User user;
+	
+	@ManyToOne
+	private Post parent;
+	
+	@OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+	private List<Post> children;
 
 }
