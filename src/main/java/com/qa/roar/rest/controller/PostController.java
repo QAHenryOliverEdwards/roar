217dc5fde8fa99fd62a3b841e74ba683d884684a
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 18:36:04 +0100

    ROR-30 #CG added create method to post controller

--- src/main/java/com/qa/roar/rest/controller/PostController.java
+++ src/main/java/com/qa/roar/rest/controller/PostController.java
@@ -1,10 +1,16 @@
 package com.qa.roar.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.roar.persistence.domain.Post;
+import com.qa.roar.rest.dto.PostDTO;
 import com.qa.roar.service.PostService;
 
 @RestController
@@ -12,12 +18,21 @@ import com.qa.roar.service.PostService;
 @CrossOrigin // this will need to be specified for security once we're set up on a server
 public class PostController {
 
-private PostService service;
+	private PostService service;
 	
 	@Autowired
 	public PostController(PostService service) {
 		super();
 		this.service = service;
+	}
+	
+	// CREATE
+	
+	@PostMapping("/create")
+	public ResponseEntity<PostDTO> create(@RequestBody Post post) {
+		return new ResponseEntity<>(
+				this.service.create(post),
+				HttpStatus.CREATED);
 	}
 	
 }
