df709e5d8d06eaaa7424d33fa13660ec27c5736e
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 18:16:57 +0100

    ROR-29 #CG added annotations, vars and constructor to user controller

--- src/main/java/com/qa/roar/rest/controller/UserController.java
+++ src/main/java/com/qa/roar/rest/controller/UserController.java
@@ -1,5 +1,25 @@
 package com.qa.roar.rest.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.roar.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin // this will need to be specified for security once we're set up on a server
 public class UserController {
+	
+	private UserService service;
+	
+	@Autowired
+	public UserController(UserService service) {
+		super();
+		this.service = service;
+	}
+	
+	
 
 }
