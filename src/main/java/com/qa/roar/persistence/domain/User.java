231c6255f30be7a91f1bdf1530ce1a03283b49e9
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 17:01:16 +0100

    ROR-21 #CG added vars to user domain

--- src/main/java/com/qa/roar/persistence/domain/User.java
+++ src/main/java/com/qa/roar/persistence/domain/User.java
@@ -1,5 +1,49 @@
 package com.qa.roar.persistence.domain;
 
-public class User {
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(max = 24)
+	@Column(unique = true)
+	private String username;
+	
+	@NotNull
+	@Size(max = 36)
+	private String name;
+	
+	@NotNull
+	@Size(max = 36)
+	@Column(unique = true)
+	private String email;
+	
+	@NotNull
+	@Size(max = 36)
+	private String password;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Post> posts;
 }
