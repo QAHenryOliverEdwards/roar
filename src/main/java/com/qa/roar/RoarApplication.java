7857b8cf960879d3731914c57c88cb27a408880e
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 14:45:19 +0100

    #CG spring project created

---
+++ src/main/java/com/qa/roar/RoarApplication.java
@@ -0,0 +1,13 @@
+package com.qa.roar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/roar/RoarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.roar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
