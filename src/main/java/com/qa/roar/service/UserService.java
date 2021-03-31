b89a7ad7797506e8bc38328ffc64b7e1f0ca3793
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 17:33:27 +0100

    ROR-27 #CG added annotations and variables and map methods to user service

--- src/main/java/com/qa/roar/service/UserService.java
+++ src/main/java/com/qa/roar/service/UserService.java
@@ -1,5 +1,31 @@
 package com.qa.roar.service;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.roar.persistence.domain.User;
+import com.qa.roar.persistence.repository.UserRepo;
+import com.qa.roar.rest.dto.UserDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserService {
 
+	private final UserRepo repo;
+	private final ModelMapper mapper;
+	
+	// map to DTO
+	private UserDTO mapToDTO(User user) {
+		return this.mapper.map(user, UserDTO.class);
+	}
+	
+	// map from DTO
+	private User mapFromDTO(UserDTO userDTO) {
+		return this.mapper.map(userDTO, User.class);
+	}
+	
+	
 }
