0520d0066dc1d56ff1bbd4ad182bbae22c4a9083
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 18:36:56 +0100

    ROR-30 #CG added read all method to post controller

--- src/main/java/com/qa/roar/rest/controller/PostController.java
+++ src/main/java/com/qa/roar/rest/controller/PostController.java
@@ -1,9 +1,12 @@
 package com.qa.roar.rest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +36,15 @@ public class PostController {
 		return new ResponseEntity<>(
 				this.service.create(post),
 				HttpStatus.CREATED);
+	}
+
+	// READ
+	
+	@GetMapping("/read")
+	public ResponseEntity<List<PostDTO>> read() {
+		return new ResponseEntity<>(
+				this.service.read(),
+				HttpStatus.OK);
 	}
 	
 }
