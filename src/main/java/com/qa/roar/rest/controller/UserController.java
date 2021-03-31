c0b1674e1ffdad4073dbee881a80bcac6ba942a1
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 18:32:36 +0100

    ROR-29 #CG added delete method to user controller

--- src/main/java/com/qa/roar/rest/controller/UserController.java
+++ src/main/java/com/qa/roar/rest/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,6 +74,12 @@ public class UserController {
 	}
 	
 	// DELETE
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<UserDTO> update( @PathVariable Long id) {
+		return this.service.delete(id) ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 	
 	// LOGIN
 	
