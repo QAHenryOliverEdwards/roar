ed72ae3b64fbadc2437613a2f493b8d00ebc7055
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 18:40:14 +0100

    ROR-30 #CG added delete method to post controller

--- src/main/java/com/qa/roar/rest/controller/PostController.java
+++ src/main/java/com/qa/roar/rest/controller/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,6 +64,14 @@ public class PostController {
 		return new ResponseEntity<>(
 				this.service.update(post, id),
 				HttpStatus.ACCEPTED);
+	}
+	
+	// DELETE
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<PostDTO> update( @PathVariable Long id) {
+		return this.service.delete(id) ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 }
