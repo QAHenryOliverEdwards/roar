099a98a2014dd7ee87f704c68e9bed100279b98a
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 17:29:19 +0100

    ROR-26 #CG set up post repo

--- src/main/java/com/qa/roar/persistence/repository/PostRepo.java
+++ src/main/java/com/qa/roar/persistence/repository/PostRepo.java
@@ -1,5 +1,11 @@
 package com.qa.roar.persistence.repository;
 
-public class PostRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qa.roar.persistence.domain.Post;
+
+@Repository
+public interface PostRepo extends JpaRepository<Post, Long>{
 
 }
