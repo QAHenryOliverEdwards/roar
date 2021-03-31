5da3985273dd8b61b7ff182aa63ab8a0f073412c
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 18:21:10 +0100

    ROR-29 #CG added read all method to user controller

--- src/main/java/com/qa/roar/rest/controller/UserController.java
+++ src/main/java/com/qa/roar/rest/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.qa.roar.rest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +39,13 @@ public class UserController {
 	}
 	
 	// READ
+	
+	@GetMapping("/read")
+	public ResponseEntity<List<UserDTO>> read() {
+		return new ResponseEntity<>(
+				this.service.read(),
+				HttpStatus.OK);
+	}
 	
 	// UPDATE
 	
