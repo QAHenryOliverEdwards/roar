821dc9d055344b4a753b10e7434f1ae33b1c9418
CGuthrieQA <CGuthrie@QA.com>
2021-03-31 18:37:32 +0100

    ROR-30 #CG added read by id method to post controller

--- src/main/java/com/qa/roar/rest/controller/PostController.java
+++ src/main/java/com/qa/roar/rest/controller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,6 +46,11 @@ public class PostController {
 		return new ResponseEntity<>(
 				this.service.read(),
 				HttpStatus.OK);
+	}
+	
+	@GetMapping("/read/{id}")
+	public ResponseEntity<PostDTO> read(@PathVariable Long id) {
+		return ResponseEntity.ok(this.service.read(id)); // alternate OK because why not?
 	}
 	
 }
