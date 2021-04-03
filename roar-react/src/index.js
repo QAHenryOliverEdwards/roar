430a7a554e82df85610757673d77a0912872d88b
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-04-03 15:12:47 +0100

    Removed raw react code

--- roar-react/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './css/bootstrap.css';
-import './css/roar-main-page.css';
-
-ReactDOM.render(<App/>, document.getElementById('root'));
--- roar-react/src/main page components/Topbar/Topbar.js
+++
@@ -1,9 +0,0 @@
-import {Searchbar} from "./Searchbar";
-
-export const Topbar =(props)=>{
-    const {searchText, searchEvent} = props;
-    console.log(searchEvent);
-    return (
-        <Searchbar searchText={searchText} searchEvent={searchEvent}/>
-    )
-}
