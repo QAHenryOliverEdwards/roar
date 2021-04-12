fc1651b2cef027b6435ff085d4585a5f5fc18860
CGuthrieQA <CGuthrie@QA.com>
2021-04-12 14:32:04 +0100

    ROR-10 #CG added create to user controller unit test

--- src/test/java/com/qa/roar/rest/controller/UserControllerUnitTest.java
+++ src/test/java/com/qa/roar/rest/controller/UserControllerUnitTest.java
@@ -1,11 +1,20 @@
 package com.qa.roar.rest.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.qa.roar.persistence.domain.User;
 import com.qa.roar.rest.dto.UserDTO;
@@ -29,6 +38,20 @@ public class UserControllerUnitTest {
 	
 	private UserDTO mapToDTO(User user) {
 		return this.mapper.map(user, UserDTO.class);
+	}
+	
+	@Test
+	public void createUserTest() {
+		
+		when(this.service.create(testUser)).thenReturn(this.mapToDTO(testUser));
+		
+		ResponseEntity <UserDTO> expected = new ResponseEntity<UserDTO>(this.mapToDTO(testUser), HttpStatus.CREATED);
+		ResponseEntity <UserDTO> result = this.controller.create(testUser);
+		
+		assertNotNull(result);
+		assertEquals(expected, result);
+		
+		verify(this.service, atLeastOnce()).create(testUser);
 	}
 	
 }
