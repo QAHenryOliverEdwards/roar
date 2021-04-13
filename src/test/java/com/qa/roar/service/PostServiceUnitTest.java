5992844d5ba77f1defb15c892dd7b79b115b1719
CGuthrieQA <CGuthrie@QA.com>
2021-04-13 14:42:14 +0100

    ROR-10 #CG added create to post service unit test

--- src/test/java/com/qa/roar/service/PostServiceUnitTest.java
+++ src/test/java/com/qa/roar/service/PostServiceUnitTest.java
@@ -1,7 +1,13 @@
 package com.qa.roar.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,4 +38,19 @@ public class PostServiceUnitTest {
 	
 	private final List<Post> testPostList = List.of(testPost1, testPost2);
 	
+	@Test
+	void createPostTest() {
+		
+		PostDTO expected = this.mapToDTO(testPost1);
+		
+		when(this.repo.save(testPost1)).thenReturn(testPost1);
+		
+		PostDTO result = this.service.create(testPost1);
+		
+		assertEquals(expected, result);
+		
+		verify(this.repo, atLeastOnce()).save(testPost1);
+		
+	}
+
 }
