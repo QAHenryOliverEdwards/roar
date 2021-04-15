4e493d03b1f5ab1baabd9b2b1141eb2e20c1edab
CGuthrieQA <CGuthrie@QA.com>
2021-04-15 14:26:39 +0100

    ROR-10 #CG added constructors and vars to user service unit test

---
+++ src/test/java/com/qa/roar/service/UserServiceUnitTest.java
@@ -0,0 +1,33 @@
+package com.qa.roar.service;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.qa.roar.persistence.domain.User;
+import com.qa.roar.persistence.repository.PostRepo;
+import com.qa.roar.rest.dto.UserDTO;
+
+public class UserServiceUnitTest {
+	
+	@Autowired
+	private UserService service;
+	
+	@MockBean
+	private PostRepo repo;
+	
+	@Autowired
+	private ModelMapper mapper;
+	
+	private UserDTO mapToDTO(User user) {
+		return this.mapper.map(user, UserDTO.class);
+	}
+	
+	private final User testUser2 = new User();
+	private final User testUser3 = new User();
+	
+	private final List<User> testUserList = List.of(testUser2, testUser3);
+
+}
